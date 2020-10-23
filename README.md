fccf7b3ca232467b2e465fc0a5c544b2ffc93f22
Genshi0916 <genshi0916@gmail.com>
2020-10-24 00:10:59 +0900

    [追加]動くようにしました

---
+++ README.md
@@ -0,0 +1,21 @@
+# genshi-contribution-check
+ 
+自分がその日コミットしていなかったら#contributter_reportを利用して煽り散らす自分用のTwitter Botです
+
+# Requirement
+ 
+* python 3.8.5
+* pip 20.2.3
+ 
+# Installation
+ 
+```bash
+$ pip install tweepy
+```
+# Important points
+key.py must be run with your API key and an access token
+ 
+# Usage
+```bash
+$ python3 twitter_bot.py
+```
---
+++ twitter_bot.py
@@ -0,0 +1,28 @@
+import tweepy
+import datetime,sys,time
+from key import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN_KEY,ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+tmp = 'https://twitter.com/genshi0916/status/'
+
+while True:
+  now = datetime.datetime.now()
+  # print(str(now.hour)+":"+str(now.minute)+" "+str(now.second))
+
+  if now.hour == 0 and now.minute == 30 and now.second == 0:
+    for tweet in tweepy.Cursor(api.search, q='genshi0916 contribution 0').items(1):
+      id = tweet.id
+      # user_name = tweet.user.screen_name
+      # tmp = 'https://twitter.com/'+user_name+'/status/'
+
+      try:
+        print("contribution数:0ってどういうことですか?????????????進捗出してください\n" + tmp + str(id))
+        time.sleep(86400) #だいたい1日待つ
+
+        api.update_status("contribution数:0ってどういうことですか?????????????進捗出してください\n" + tmp + str(id))
+
+      except:
+        print('error')
