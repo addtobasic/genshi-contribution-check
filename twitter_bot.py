fc48e1f2f14741d639888617a840d145531c9c55
Genshi0916 <genshi0916@gmail.com>
2020-10-25 13:29:20 +0900

    [修正]しっかりとコミットしている場合最後のコミット0が拾われてしまうのを修正

--- twitter_bot.py
+++ twitter_bot.py
@@ -10,9 +10,8 @@ tmp = 'https://twitter.com/genshi0916/status/'
 while True:
   now = datetime.datetime.now()
   # print(str(now.hour)+":"+str(now.minute)+" "+str(now.second))
-
   if now.hour == 0 and now.minute == 15 and now.second == 0:
-    for tweet in tweepy.Cursor(api.search, q='genshi0916 contribution 0').items(1):
+    for tweet in tweepy.Cursor(api.search, q='genshi0916 contribution 0 '+str(now.day)).items(1):
       id = tweet.id
       # user_name = tweet.user.screen_name
       # tmp = 'https://twitter.com/'+user_name+'/status/'
@@ -20,7 +19,7 @@ while True:
       try:
         print("contribution数:0ってどういうことですか?????????????進捗出してください\n" + tmp + str(id))
         api.update_status("contribution数:0ってどういうことですか?????????????進捗出してください\n" + tmp + str(id))
+        print("----sleep中です----")
         time.sleep(86000) #だいたい1日待つ
       except:
         print('error')
-
